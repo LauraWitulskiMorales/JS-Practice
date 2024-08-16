e7c8152b881d23cc1a8c9587459195fabd3cb605
Laura Witulski Morales <l.witulski.morales@accenture.com>
2024-08-16 17:41:15 +0200

    The Sum of a Range (Bonus)

--- sum.js
+++ sum.js
@@ -14,3 +14,18 @@ function range (start, end) {
     return total;
   }
       
+
+  // Bonus
+  function range (start, end, step = 1) {
+    let result = [];
+    if (step > 0) {
+      for (let i = start; i <= end; i++) {
+        result.push(i);
+      }
+   } else {
+      for (let i = start; i >= end; i += step) {
+        result.push(i)
+      }
+    }
+    return result;
+  }
